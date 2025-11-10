e487468171b1b5b9956378769138ff1eda1c566d
SamiaFykri <samiafykri06@gmail.com>
2025-11-10 09:24:08 +0100

    V2

--- src/main/java/org/sid/tpgraphql/model/Centre.java
+++ src/main/java/org/sid/tpgraphql/model/Centre.java
@@ -1,14 +1,10 @@
 package org.sid.tpgraphql.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
